b0bb6806cfa83a0861aab236cc6dc5da5a783bda
AbhiChavanoo3 <133866600+AbhiChavanoo3@users.noreply.github.com>
2025-06-13 14:02:54 +0530

    Create ReverseString.js

---
+++ ReverseString.js
@@ -0,0 +1,8 @@
+function reverseString (str) {
+   let reversed = ""
+   for (i=str.length-1;i>=0;i--) {
+      reversed += str[i];
+   } 
+   return reversed;
+}
+console.log(reverseString("Hello World"));
